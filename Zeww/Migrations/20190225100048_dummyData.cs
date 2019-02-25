ba88101e1430736afe6fbf7faa0773ff6505c6e6
Esraa ElMalah <Esraa.ElMalah@itworx.com>
2019-02-25 13:38:46 +0200

    Commit with new migrations

---
+++ Zeww/Migrations/20190225100048_dummyData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zeww.Migrations
+{
+    public partial class dummyData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
