bc71d2721bc11467bc4759c9ddad3e7641bfe2c0
Marc Iskander <Marc.Iskander@itworx.com>
2019-02-28 11:32:24 +0200

    added attribute isStarred in UserChats

--- Zeww/Models/UserChats.cs
+++ Zeww/Models/UserChats.cs
@@ -10,10 +10,8 @@ namespace Zeww.Models
     {
         [Key]
         public int UserId { get; set; }
-        public User User { get; set; }
         [Key]
         public int ChatId { get; set; }
-        public Chat Chat { get; set; }
         public bool IsStarred { get; set; }
     }
 }
